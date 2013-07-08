cf56e8c434187d0a092d5547c07fa9e42d480c59
Eriprando Pacces <eriprando.pacces@gmail.com>
2013-07-08 11:23:30 +0200

    adds tests for EPUserPosition class

--- EPMapExtensions/EPUserPosition.m
+++ EPMapExtensions/EPUserPosition.m
@@ -15,23 +15,30 @@ static inline BOOL isHorizontalAccuracyWithinARange(CLLocation *location, CLLoca
     return location.horizontalAccuracy >= 0 && location.horizontalAccuracy <= range;
 }
 
+static inline BOOL isValidLocation(CLLocation *location)
+{
+    return location.horizontalAccuracy >= 0 && CLLocationCoordinate2DIsValid(location.coordinate);
+}
+
 @implementation EPUserPosition
 
 - (id)initWithUserPosition:(CLLocation *)userLocation
 {
     if (self = [super init]) {
         _minThresholdToUpdatePosition = TSMinimumDefaultThresholdToUpdateUserPosition;
-        _userPosition = userLocation;
+        [self updatePosition:userLocation];
     }
     return self;
 }
 
 - (void)updatePosition:(CLLocation *)newUserPosition
 {
-    if (!newUserPosition) return;
-    if(isHorizontalAccuracyWithinARange(newUserPosition, _minThresholdToUpdatePosition)) {
-        if ([newUserPosition distanceFromLocation:_userPosition] > _minThresholdToUpdatePosition)
-            _userPosition = newUserPosition;
+    if (!isValidLocation(newUserPosition)) return;
+    
+    if (!_userPosition) {
+        _userPosition = newUserPosition; 
+    } else if([self locationNeedsUpdate:newUserPosition]){
+        _userPosition = newUserPosition;
     }
 }
 
@@ -53,8 +60,7 @@ static inline BOOL isHorizontalAccuracyWithinARange(CLLocation *location, CLLoca
 - (BOOL)locationNeedsUpdate:(CLLocation *)newLocation
 {
     return (isHorizontalAccuracyWithinARange(newLocation, _minThresholdToUpdatePosition) &&
-            [newLocation distanceFromLocation:_userPosition] >= _minThresholdToUpdatePosition &&
-            YES
+            [newLocation distanceFromLocation:_userPosition] >= _minThresholdToUpdatePosition
             );
 }
 
--- EPMapExtensionsTests/EPUserPositionTest.m
+++ EPMapExtensionsTests/EPUserPositionTest.m
@@ -37,24 +37,58 @@
     CLLocationCoordinate2D coord1 = _simulatedUserLocation.coordinate;
     CLLocationCoordinate2D coord2 = _simulatedUserPosition.coordinate;
     
-    STAssertEquals(coord1.latitude, coord2.latitude, @"latitudes should be equal");
-    STAssertEquals(coord1.longitude, coord2.longitude, @"latitudes should be equal");
+    STAssertTrue(coord1.latitude != coord2.latitude && coord1.longitude != coord2.longitude, @"latitudes and longitudes should be different");
+    STAssertTrue(coord2.latitude == 0 && coord2.longitude == 0, @"latitudes & longitude should be zero");
 }
 
 - (void)testUpdateUserPositionWithValidPosition
 {
+    _simulatedUserLocation = [[CLLocation alloc] initWithLatitude:12323.11 longitude:212812.67];
+    _simulatedUserPosition = [[EPUserPosition alloc] initWithUserPosition:_simulatedUserLocation];
     
+    [_simulatedUserPosition updatePosition:[[CLLocation alloc] initWithLatitude:45.11 longitude:7.67]];
+    
+    STAssertTrue(_simulatedUserPosition.coordinate.longitude == 7.67 && _simulatedUserPosition.coordinate.latitude == 45.11,
+                 @"latitude should be 45.11 and logitude should be 7.67");
+
 }
 
 - (void)testUpdateUserPositionWithInvalidPosition
 {
+    _simulatedUserLocation = [[CLLocation alloc] initWithLatitude:45.11 longitude:7.67];
+    _simulatedUserPosition = [[EPUserPosition alloc] initWithUserPosition:_simulatedUserLocation];
+    
+    [_simulatedUserPosition updatePosition:[[CLLocation alloc] initWithLatitude:1232432.4343 longitude:-212331.3]];
+    
+    STAssertTrue(_simulatedUserPosition.coordinate.longitude == 7.67 && _simulatedUserPosition.coordinate.latitude == 45.11,
+                 @"latitude should be 45.11 and logitude should be 7.67");
     
 }
 
 - (void)testUpdateUserPositionWithValidPositionBelowThreshold
 {
+    _simulatedUserLocation = [[CLLocation alloc] initWithLatitude:45.11 longitude:7.67];
+    _simulatedUserPosition = [[EPUserPosition alloc] initWithUserPosition:_simulatedUserLocation];
     
+    _simulatedUserPosition.minThresholdToUpdatePosition = 2000.0;
+    
+    [_simulatedUserPosition updatePosition:[[CLLocation alloc] initWithLatitude:45.1112 longitude:7.67014]];
+    
+    STAssertTrue(_simulatedUserPosition.coordinate.longitude == 7.67 && _simulatedUserPosition.coordinate.latitude == 45.11,
+                 @"latitude should be 45.11 and logitude should be 7.67");
 }
 
+- (void)testUpdateUserPositionWithValidPositionBeyondThreshold
+{
+    _simulatedUserLocation = [[CLLocation alloc] initWithLatitude:45.11 longitude:7.67];
+    _simulatedUserPosition = [[EPUserPosition alloc] initWithUserPosition:_simulatedUserLocation];
+    
+    _simulatedUserPosition.minThresholdToUpdatePosition = 2000.0;
+    
+    [_simulatedUserPosition updatePosition:[[CLLocation alloc] initWithLatitude:45.1812 longitude:7.67014]];
+    
+    STAssertTrue(_simulatedUserPosition.coordinate.longitude == 7.67014 && _simulatedUserPosition.coordinate.latitude == 45.1812,
+                 @"latitude should be 45.11 and logitude should be 7.67");
+}
 
 @end
